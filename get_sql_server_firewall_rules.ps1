6a293a6c217fda239a8e29e54375f984e331e080
Kevin Qiu <kevin.qiu@jet.com>
2016-10-22 14:44:11 -0400

    adding scripts for azure sql server

---
+++ get_sql_server_firewall_rules.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount
+$subscriptions = (Get-AzureRmSubscription).subscriptionname
+"Subscription,Resource Group,Server Name,Start IP Address,End IP Address,Firewall Rule Name" | Add-Content 'sql_server_firewall_rules.csv' 
+foreach ($subscription in $subscriptions) {
+    echo("==============================================")
+    echo $("Fetching SQL Servers for " + $subscription)
+    echo("==============================================")
+    Get-AzureRmSubscription -SubscriptionName $subscription | Set-AzureRmContext | Out-Null
+    foreach ($sqlServer in Get-AzureRmResourceGroup | Get-AzureRmSqlServer)
+    {
+        foreach ($firewallRule in Get-AzureRmSqlServerFirewallRule -ResourceGroupName $sqlServer.ResourceGroupName -ServerName $sqlServer.ServerName) {
+            
+            
+            $outputLine = $subscription + "," + $sqlServer.ResourceGroupName + "," + $sqlServer.ServerName + "," + $firewallRule.StartIpAddress + "," + $firewallRule.EndIpAddress + "," + $firewallRule.FirewallRuleName
+            $outputLine | Out-File -append 'sql_server_firewall_rules.csv' 
+        }
+    }
+    #(Get-AzureRmRoleAssignment -IncludeClassicAdministrators | Where-Object RoleDefinitionName -like "*administrator*").SignInName | Out-File $("admins/" + $subscription + "_admins.txt")
+}
---
+++ get_sql_servers.ps1
@@ -0,0 +1,21 @@
+﻿Login-AzureRmAccount
+$subscriptions = (Get-AzureRmSubscription).subscriptionname
+"Subscription,Resource Group,Server Name,Database Name" | Add-Content 'sql_server_output.csv' 
+foreach ($subscription in $subscriptions) {
+    echo("==============================================")
+    echo $("Fetching SQL Servers for " + $subscription)
+    echo("==============================================")
+    Get-AzureRmSubscription -SubscriptionName $subscription | Set-AzureRmContext | Out-Null
+    foreach ($sqlServer in Get-AzureRmResourceGroup | Get-AzureRmSqlServer)
+    {   
+        foreach ($sqlDatabase in Get-AzureRmSqlDatabase -ResourceGroupName $SqlServer.ResourceGroupName -ServerName $SqlServer.ServerName) {   
+            try { 
+                $outputLine = $subscription + "," + $sqlServer.ResourceGroupName + "," + $sqlServer.ServerName + "," + $sqlDatabase.DatabaseName
+                $outputLine | Out-File -append 'sql_server_output.csv'
+            } 
+            catch {
+
+            }
+        }
+    }
+}
