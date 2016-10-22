6d17a762b3701ce6dba57b7b1dcda2afcd672f01
Kevin Qiu <kevin.qiu@jet.com>
2016-10-22 14:39:42 -0400

    adding azure fetch admins script

---
+++ get_azure_admins.ps1
@@ -0,0 +1,8 @@
+﻿Login-AzureRmAccount
+$subscriptions = (Get-AzureRmSubscription).subscriptionname
+foreach ($subscription in $subscriptions) {
+    echo $("Fetching admins for " + $subscription)
+    Get-AzureRmSubscription -SubscriptionName $subscription | Set-AzureRmContext | Out-Null
+    (Get-AzureRmRoleAssignment -IncludeClassicAdministrators | Where-Object RoleDefinitionName -like "*administrator*").SignInName | Out-File $("admins/" + $subscription + "_admins.txt")
+}
+echo "Done!"
