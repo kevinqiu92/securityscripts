1139d4b8fafee790604bdf781b7f48156adac8bc
Kevin Qiu <kevin.qiu@kqiu-m2.Winterfell>
2017-04-03 22:03:22 -0400

    uploading nsg rules fetcher

---
+++ get_nsg_rules.ps1
@@ -0,0 +1,11 @@
+﻿Login-AzureRmAccount
+$subscriptions = (Get-AzureRmSubscription).subscriptionname
+"Subscription,NSG Name,Rule Name,Protocol,Source Port,Destination Port,Source Address,Destination Address,Direction" | Add-Content 'azure_nsgs.csv'
+foreach ($subscription in $subscriptions) {
+    Get-AzureRmSubscription -SubscriptionName $subscription | Set-AzureRmContext | Out-Null
+    foreach ($nsg in Get-AzureRmNetworkSecurityGroup) {
+        foreach ($rule in $nsg.SecurityRules) {
+            $subscription + "," + $nsg.Name + "," + $rule.Name + "," + $rule.Protocol + "," + $rule.SourcePortRange + "," + $rule.DestinationPortRange + "," + $rule.SourceAddressPrefix + "," + $rule.DestinationAddressPrefix + "," + $rule.Direction| Add-Content 'azure_nsgs.csv'
+        }
+    }
+}
